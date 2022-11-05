18b0cd4db39e6b09e4c3ce2f4ffd3e911416a716
romanparadise <108572087+romanparadise@users.noreply.github.com>
2022-11-06 00:27:15 +0400

    fuck cors

--- src/containers/Sections/BotsNProxies/index.js
+++ src/containers/Sections/BotsNProxies/index.js
@@ -73,7 +73,8 @@ const BotsNProxies = () => {
     var requestOptions = {
       method: 'POST',
       body: formdata,
-      redirect: 'follow'
+      redirect: 'follow',
+      mode: "no-cors",
     };
     
     fetch(`${process.env.REACT_APP_API_BASE_URL}/uploadBots`, requestOptions)
